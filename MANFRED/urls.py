8a21ea01c188893a8eced2934d58ceeb822b82a9
tuvaman <89556100+tuvaman@users.noreply.github.com>
2021-12-11 00:54:54 +0300

    Update urls.py

--- MANFRED/urls.py
+++ MANFRED/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('skills',views.skills,name='skills'),
 ]
 
-if  settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+ urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
