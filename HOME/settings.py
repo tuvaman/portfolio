7fc008ef4a743cf17d8233d990adc408d41b0107
tuvaman <89556100+tuvaman@users.noreply.github.com>
2021-12-11 01:28:51 +0300

    Update settings.py

--- HOME/settings.py
+++ HOME/settings.py
@@ -132,7 +132,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 # Static files (CSS, JavaScript, Images)
